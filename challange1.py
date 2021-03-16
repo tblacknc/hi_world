9cf9a0091e8bbbfd520558b473cac7887b3eb422
tblacknc <tblacknc@gmail.com>
2021-03-15 23:20:18 -0400

    added tests

--- challange1.py
+++ challange1.py
@@ -70,3 +70,14 @@ def arithmetic_arranger(problems, *true):
     arranged_problems = arranged_problems.rstrip()
 
     return arranged_problems
+
+
+print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
+print("")
+print(arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]))
+print("")
+print(arithmetic_arranger(["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"]))
+print("")
+print(arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"]))
+print("")
+print(arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49"], True))
