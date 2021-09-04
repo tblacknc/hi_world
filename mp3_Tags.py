4b05a0d22031e278fade8d8fa2ed58fafb8ca69b
tblacknc <tblacknc@gmail.com>
2021-09-04 10:38:56 -0400

    Update mp3_Tags.py
    clean up

--- mp3_Tags.py
+++ mp3_Tags.py
@@ -5,22 +5,37 @@ import os
 from os import walk
 
 mypath = r'C:\Users\tblac\Desktop\iTunes Music\new'
+mypath = r'C:\Users\tblac\Desktop\latest tunes\new'
+mypath = r'C:\Users\tblac\Desktop\latest tunes'
+error_dir = "NONE"
+albm = ""
+artist = "" 
+titletag = ""
 
 for (dirpath, dirnames, filenames) in walk(mypath):
 
-    os.chdir(dirpath)
+    try :
+        os.chdir(dirpath)
+        
+    except:
+        print("must be in that dir")
+        time.sleep(2)
+        continue
+        
     cwd = os.getcwd()
     
     for filename in filenames:
         
         dirs = cwd.split("\\")
-        
         albm = dirs[-1]
         artist = dirs[-2]
                 
         if re.findall("mp3$", filename):
 
             audiofile = eyed3.load(filename)
+            titletag = re.sub("\.mp3", "", filename)
+
+            titletag = re.sub("^[0-9.-]*[0-9]*", "", titletag)
 
             try:
                 audiofile.tag.album = albm
@@ -31,22 +46,33 @@ for (dirpath, dirnames, filenames) in walk(mypath):
                 print("artist ===== ", audiofile.tag.artist)
                 print("dirAlbum ===== ", audiofile.tag.album)
                 print("*************************************************")           
-
             except:
-                error_dir = error_dir ,"\n", dirpath, "\n", filename
-                print("title ===== ", audiofile.tag.title)
-                print("artist ===== ", audiofile.tag.artist)
-                print("dirAlbum ===== ", audiofile.tag.album)
-                print("*************************************************")
+                error_dir += "tags " 
+                error_dir += str(titletag) 
+                error_dir += str(artist)
+                error_dir += str(albm)
+                error_dir += "--------------------------\n\n"
+                #error_dir += "tags____", str(artist), "____", str(albm)
+                                
+                print("title = ", titletag)
+                print("artist = ", artist)
+                print("album = ", albm)
+                print("==================================================")
                 time.sleep(2)
             
             try:
                 audiofile.tag.save()
             except:
                 print("COULDN'T SAVE audiofile.tag.save...... DRATS")
-                print("title ===== ", audiofile.tag.title)
-                print("artist ===== ", audiofile.tag.artist)
-                print("dirAlbum ===== ", audiofile.tag.album)
-                print("*************************************************")
-                time.sleep(2)
+                error_dir += "Files " 
+                error_dir += str(titletag) 
+                error_dir += str(artist)
+                error_dir += str(albm)
+                error_dir += "--------------------------\n\n"
                 
+                print("title = ", titletag)
+                print("artist = ", artist)
+                print("album = ", albm)
+                print("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*")
+                time.sleep(2)
+    print(error_dir)                
